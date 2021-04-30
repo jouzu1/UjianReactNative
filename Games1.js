990496fc40b0c464bc08114d81cc286a85c6f25a
jouzu1 <jouzu1@gmail.com>
2021-04-30 19:58:32 +0700

    Mengembalikkan dari jempol bawah ke atas lagi

--- Games1.js
+++ Games1.js
@@ -12,19 +12,32 @@ export class Games1 extends Component {
 
     onButtonPress = () => {
         this.setState({
-          foto: require('./src/image/248068_thumbs-down-youtube-thumbs-up-png-transparent-png.png')
+          foto: require('./src/image/png-transparent-thumbs-down-art-thumb-signal-emoji-symbol-give-a-thumbs-up-hand-smiley-arm.png')
         });
       }
 
+      
+    onButtonPressAgain = () => {
+        const test = [];
+        for (let i = 0; i < 50; i++) {
+            test.push(<TouchableOpacity onPress={this.setState({foto:require('./src/image/thumb-up-cartoon-png_246633.jpg')})}>
+            </TouchableOpacity>);
+            console.log(test)
+}
+        // this.setState({
+        //   foto: require('./src/image/thumb-up-cartoon-png_246633.jpg')
+        // });
+      }
+
+
+
     render() {
         const test = [];
-
-
-
         for (let i = 0; i < 50; i++) {
                     test.push(<TouchableOpacity onPress={this.onButtonPress}>
                         <Image source={this.state.foto} style={style.image}></Image>
                     </TouchableOpacity>);
+                    console.log(test)
         }
 
 
@@ -32,6 +45,7 @@ export class Games1 extends Component {
             <View>
                 <Text>
                     {test}
+                    {/* {biarBanyak()} */}
                 </Text>
                 {/* <Text> textInComponent </Text>   */}
                 {/* <TouchableOpacity>
@@ -49,6 +63,7 @@ export class Games1 extends Component {
                 <TouchableOpacity >
                     <Image source={require('./src/image/thumb-up-cartoon-png_246633.jpg')} style={style.image}></Image>
                 </TouchableOpacity> */}
+                 <Button title="Reset" onPress={() => { this.onButtonPressAgain() }} />
                 <Button title="Lanjut Ke Games 2" onPress={() => { this.props.navigation.navigate("Games2") }} />
             </View>
         )
@@ -60,7 +75,7 @@ export default Games1
 const style = StyleSheet.create({
     image: {
         height: 50,
-        width: 50,
+        width:50,
         // flexDirection:'row', 
         // flex:1
     }
